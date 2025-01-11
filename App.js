d749e13112eb93dbc0edeb876ced9aaee9d874c9
snadi043 <snadi043@uottawa.ca>
2025-01-11 11:25:34 -0500

    Implemented Stack navigation in the App.js file as per the requirements.

--- App.js
+++ App.js
@@ -1,18 +1,53 @@
+import './gesture-handler';
+
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import AllPlacesScreen from './screens/All_Places';
 import AddPlacesScreen from './screens/Add_Places';
+import IconButton from './components/UI/IconButton';
+
+import { Colors } from './constants/colors';
 
 export default function App() {
   const Stack = createStackNavigator();
+  // const navigation = useNavigation();
   return (
     <>
       <StatusBar style='dark'/>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="AllPlaces" component={AllPlacesScreen}></Stack.Screen>
-          <Stack.Screen name="AddPlace" component={AddPlacesScreen}></Stack.Screen>
+        <Stack.Navigator screenOptions={{
+          headerStyle: {backgroundColor: Colors.primary700},
+          headerTintColor: Colors.gray700,
+          cardStyle: {backgroundColor: Colors.gray700}
+          
+        }}>
+          <Stack.Screen 
+            name="AllPlaces" 
+            component={AllPlacesScreen}
+            options={
+              ({ navigation }) => ({
+              title: 'Your Favorite Places',
+              headerRight: ({tintColor}) => (
+                <IconButton
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                  onPress={() => {
+                    navigation.navigate('AddPlace')
+                  }
+                  }
+                />
+              ),
+            })
+            }
+            ></Stack.Screen>
+          <Stack.Screen
+            name="AddPlace" 
+            component={AddPlacesScreen}
+            options={{
+              title: 'Add new place'
+            }}></Stack.Screen>
         </Stack.Navigator>
       </NavigationContainer>
     </>
